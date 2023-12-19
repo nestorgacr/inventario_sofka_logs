bd6f55cc73cfc5f66c8b5cb8f61b589471e897b6
Nestor Garcia <ngarcia@ologistics.com>
2023-12-19 14:14:06 -0600

    estructura inicial

---
+++ src/main/java/com/epa/inventarioLog/InventarioLogApplication.java
@@ -0,0 +1,13 @@
+package com.epa.inventarioLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioLogApplication.class, args);
+	}
+
+}
